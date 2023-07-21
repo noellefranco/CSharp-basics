2d3bbed3b32057f6a5752621d40be50de5bb8303
noelle <noelle.mtf@gmail.com>
2023-07-21 00:25:06 -0300

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+
+    public IActionResult Welcome(string name, int numTimes = 1) {
+        ViewData["Message"] = "Olá, " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
